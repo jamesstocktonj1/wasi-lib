9945b37c5fe92f47e13eae64c42f9a74fad91522
jamesstocktonj1 <james.stocktonj@gmail.com>
2025-03-08 12:05:17 +0000

    Add basic crud example

--- example/keyvalue-crud/go.mod
+++ example/keyvalue-crud/go.mod
@@ -4,7 +4,11 @@ go 1.23.0
 
 toolchain go1.23.7
 
+replace github.com/jamesstocktonj1/wasi-lib => ../../
+
 require (
+	github.com/jamesstocktonj1/wasi-lib v0.0.0-20250308110010-3e2202d9ad48
+	github.com/julienschmidt/httprouter v1.3.0
 	go.bytecodealliance.org v0.5.0
 	go.bytecodealliance.org/cm v0.1.0
 	go.wasmcloud.dev/component v0.0.5
@@ -16,9 +20,12 @@ require (
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/regclient/regclient v0.7.2 // indirect
+	github.com/samber/lo v1.47.0 // indirect
+	github.com/samber/slog-common v0.17.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/ulikunitz/xz v0.5.12 // indirect
 	github.com/urfave/cli/v3 v3.0.0-beta1 // indirect
 	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
--- example/keyvalue-crud/main.go
+++ example/keyvalue-crud/main.go
@@ -2,19 +2,113 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
+	"github.com/jamesstocktonj1/wasi-lib/pkg/keyvalue"
+	"github.com/julienschmidt/httprouter"
+	"go.wasmcloud.dev/component/log/wasilog"
 	"go.wasmcloud.dev/component/net/wasihttp"
 )
 
+var logger = wasilog.DefaultLogger
+
 func init() {
-	// Register the handleRequest function as the handler for all incoming requests.
-	wasihttp.HandleFunc(handleRequest)
+	router := httprouter.New()
+	router.POST("/document/:document", create)
+	router.GET("/document/:document", read)
+	router.DELETE("/document/:document", del)
+	wasihttp.Handle(router)
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from Go!\n")
+func create(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "  ")
+
+	doc := ps.ByName("document")
+	logger.Info("handle create", "document", doc)
+
+	bucket, err := keyvalue.Open("store")
+	if err != nil {
+		logger.Error("keyvalue.Open", "error", err)
+		enc.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = bucket.Set(doc, "Hello World!")
+	if err != nil {
+		logger.Error("bucket.Set", "error", err)
+		enc.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	enc.Encode(map[string]string{
+		"message": "handle create",
+	})
+}
+
+func read(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "  ")
+
+	doc := ps.ByName("document")
+	logger.Info("handle read", "document", doc)
+
+	bucket, err := keyvalue.Open("store")
+	if err != nil {
+		logger.Error("keyvalue.Open", "error", err)
+		enc.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	value, err := bucket.Get(doc)
+	if err != nil {
+		logger.Error("bucket.Get", "error", err)
+		enc.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	enc.Encode(map[string]string{
+		"message": value,
+	})
+}
+
+func del(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "  ")
+
+	doc := ps.ByName("document")
+	logger.Info("handle delete", "document", doc)
+
+	bucket, err := keyvalue.Open("store")
+	if err != nil {
+		logger.Error("keyvalue.Open", "error", err)
+		enc.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = bucket.Delete(doc)
+	if err != nil {
+		logger.Error("bucket.Delete", "error", err)
+		enc.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	enc.Encode(map[string]string{
+		"message": "handle delete",
+	})
 }
 
 // Since we don't run this program like a CLI, the `main` function is empty. Instead,
